21c6bc86e747107d12069ee14b42b9e2a5a64f33
yukidaruma <yukidaruma@users.noreply.github.com>
2021-01-09 21:47:32 +0900

    Fix Vuetify was not properly initialized on CodeSandbox

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import App from './App.vue';
+import 'vuetify/dist/vuetify.min.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
 
 new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  vuetify: new Vuetify(),
+  render: (h) => h(App),
+}).$mount('#app');
